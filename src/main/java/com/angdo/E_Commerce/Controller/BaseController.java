c78458936b5267ffe47327f4c1a7bcaf970f07f9
kyd1625 <rladudeh98@naver.com>
2024-04-07 22:43:00 +0900

    20240407 kyd Front-End 수정

--- src/main/java/com/angdo/E_Commerce/Controller/BaseController.java
+++ src/main/java/com/angdo/E_Commerce/Controller/BaseController.java
@@ -9,6 +9,9 @@ public class BaseController {
 
     @GetMapping("/homepage")
     public String mainPage(){
-        return "index.html";
+        return "index";
     }
+
+    @GetMapping("/dashboard/home") // 테스트용 변경 예정 kyd 20240407
+    public String dashboardHome(){ return "dashboard/dashboard-index";}
 }
